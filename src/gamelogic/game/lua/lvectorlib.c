23ddff9dcdb7d244eafb4ac648c995d9e1f0fc9b
Jeff Kent <jeff@jkent.net>
2013-09-12 02:03:27 -0500

    header, formatting, setter fix

--- src/gamelogic/game/lua/lvectorlib.c
+++ src/gamelogic/game/lua/lvectorlib.c
@@ -1,3 +1,37 @@
+/*
+===========================================================================
+
+Daemon GPL Source Code
+Copyright (C) 2013 Jeff Kent <jeff@jkent.net>
+
+This file is part of the Daemon GPL Source Code (Daemon Source Code).
+
+Daemon Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Daemon Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Daemon Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+In addition, the Daemon Source Code is also subject to certain additional terms.
+You should have received a copy of these additional terms immediately following the
+terms and conditions of the GNU General Public License which accompanied the Daemon
+Source Code.  If not, please request a copy in writing from id Software at the address
+below.
+
+If you have questions concerning this license or the applicable additional terms, you
+may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville,
+Maryland 20850 USA.
+
+===========================================================================
+*/
+
 #define lentitylib_c
 #define LUA_LIB
 
@@ -8,15 +42,15 @@
 
 #include "../g_local.h"
 
+
 #define VECTOROBJ "vec_t*"
 
 #define tovectorobj(L, n)	((vec_t *)luaL_checkudata(L, n, VECTOROBJ))
-#define getvectordim(L, n)  (lua_rawlen(L, n) / sizeof(vec_t))
+#define getvectordim(L, n)	(lua_rawlen(L, n) / sizeof(vec_t))
+
 
 static vec_t *newvector (lua_State *L, int dim) {
-  vec_t *v;
-
-  v = (vec_t *)lua_newuserdata(L, sizeof(vec_t) * dim);
+  vec_t *v = (vec_t *)lua_newuserdata(L, sizeof(vec_t) * dim);
   memset(v, 0, sizeof(vec_t) * dim);
   luaL_setmetatable(L, VECTOROBJ);
   return v;
@@ -83,40 +117,38 @@ static int vector_call (lua_State *L) {
 
 static int vectorobj_index (lua_State *L) {
   vec_t *v = tovectorobj(L, 1);
-  int dim, i;
+  int i, dim = getvectordim(L, 1);
   const char *key;
-
-  dim = lua_rawlen(L, 1) / sizeof(vec_t);
 
   switch (lua_type(L, 2)) {
   case LUA_TSTRING:
-	key = lua_tostring(L, 2);
+    key = lua_tostring(L, 2);
 
-	/* try to fetch from metatable */
+    /* try to fetch from metatable */
     if (luaL_getmetafield(L, 1, key))
       break;
 
     /* else, try to use aliases */
-	if (dim >= 3 && strcmp("z", key) == 0)
-	  lua_pushnumber(L, (lua_Number)v[2]);
-	else if (dim >= 2 && strcmp("y", key) == 0)
-	  lua_pushnumber(L, (lua_Number)v[1]);
-	else if (dim >= 1 && strcmp("x", key) == 0)
-	  lua_pushnumber(L, (lua_Number)v[0]);
-	else
-	  lua_pushnil(L);
-	break;
+    if (dim >= 3 && strcmp("z", key) == 0)
+      lua_pushnumber(L, (lua_Number)v[2]);
+    else if (dim >= 2 && strcmp("y", key) == 0)
+      lua_pushnumber(L, (lua_Number)v[1]);
+    else if (dim >= 1 && strcmp("x", key) == 0)
+      lua_pushnumber(L, (lua_Number)v[0]);
+    else
+      lua_pushnil(L);
+    break;
 
   case LUA_TNUMBER:
-	i = lua_tointeger(L, 2) - 1;
-	if (i >= 0 && i < dim)
-	  lua_pushnumber(L, (lua_Number)v[i]);
-	else
-	  lua_pushnil(L);
-	break;
+    i = lua_tointeger(L, 2) - 1;
+    if (i >= 0 && i < dim)
+      lua_pushnumber(L, (lua_Number)v[i]);
+    else
+      lua_pushnil(L);
+    break;
 
   default:
-	lua_pushnil(L);
+    lua_pushnil(L);
 
   }
   return 1;
@@ -124,7 +156,8 @@ static int vectorobj_index (lua_State *L) {
 
 
 static int vectorobj_len (lua_State *L) {
-  int dim = lua_rawlen(L, 1) / sizeof(vec_t);
+  vec_t *v = tovectorobj(L, 1);
+  int dim = getvectordim(L, 1);
   lua_pushinteger(L, dim);
   return 1;
 }
@@ -132,20 +165,18 @@ static int vectorobj_len (lua_State *L) {
 
 static int vectorobj_newindex (lua_State *L) {
   vec_t *v = tovectorobj(L, 1);
-  int dim, i;
+  int i, dim = getvectordim(L, 1);
   const char *key;
-
-  dim = lua_rawlen(L, 1) / sizeof(vec_t);
 
   switch (lua_type(L, 2)) {
   case LUA_TSTRING:
     key = lua_tostring(L, 2);
     if (dim >= 3 && strcmp("z", key) == 0)
-      v[2] = (vec_t)lua_tonumber(L, 2);
+      v[2] = (vec_t)lua_tonumber(L, 3);
     else if (dim >= 2 && strcmp("y", key) == 0)
-      v[1] = (vec_t)lua_tonumber(L, 2);
+      v[1] = (vec_t)lua_tonumber(L, 3);
     else if (dim >= 1 && strcmp("x", key) == 0)
-      v[0] = (vec_t)lua_tonumber(L, 2);
+      v[0] = (vec_t)lua_tonumber(L, 3);
     break;
 
   case LUA_TNUMBER:
@@ -160,18 +191,18 @@ static int vectorobj_newindex (lua_State *L) {
 
 static int vectorobj_tostring (lua_State *L) {
   vec_t *v = tovectorobj(L, 1);
-  int i, dim = lua_rawlen(L, 1) / sizeof(vec_t);
+  int i, dim = getvectordim(L, 1);
   luaL_Buffer b;
 
   luaL_buffinit(L, &b);
-  lua_pushfstring(L, VECTOROBJ "[%d] (", dim);
+  lua_pushfstring(L, VECTOROBJ "[%d] {", dim);
   luaL_addvalue(&b);
   for (i = 0; i < dim; i++) {
-	if (i < dim - 1)
-	  lua_pushfstring(L, "%f, ", (lua_Number) v[i]);
-	else
-      lua_pushfstring(L, "%f)", (lua_Number) v[i]);
-	luaL_addvalue(&b);
+    if (i < dim - 1)
+      lua_pushfstring(L, "%f, ", (lua_Number) v[i]);
+    else
+      lua_pushfstring(L, "%f}", (lua_Number) v[i]);
+    luaL_addvalue(&b);
   }
   luaL_pushresult(&b);
   return 1;
